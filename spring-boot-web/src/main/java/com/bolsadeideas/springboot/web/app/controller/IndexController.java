d1e4f1fb163e2123373ada069e974ea19d2264d8
DanyWh <djblancoh@gmail.com>
2022-11-09 18:11:02 -0500

    enviar datos a las plantillas

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -2,19 +2,23 @@ package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
 //    @RequestMapping( value = "/index", method = RequestMethod.GET)
 //    @GetMapping("/index")
+//    @GetMapping({"/index", "/", "/home"})
+//    public String index(ModelMap model) {
+//        String html = "index";
+//        model.addAttribute("title", "Spring Framework!!");
     @GetMapping({"/index", "/", "/home"})
-    public String index() {
-
-
+    public ModelAndView index(ModelAndView mv) {
         String html = "index";
-        System.out.println("Testing ");
-        return html;
+        mv.addObject("title", "Spring Framework con Model and View");
+        mv.setViewName(html);
+        return mv;
     }
 
 }
