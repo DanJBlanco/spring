56641b9e729c6d66e2dab7cfa861e4277579acb2
DanWh <danielblanco@MacBook-Pro-de-Daniel.local>
2022-11-14 23:06:54 -0500

    add loop each, and model attribute ( make a object available on all controller model)

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -4,7 +4,11 @@ import com.bolsadeideas.springboot.web.app.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -31,12 +35,25 @@ public class IndexController {
         user.setName("Dan");
         user.setLastName("Wh");
 
-//        user.setEmail("d_j@g.c");
+        user.setEmail("d_j@g.c");
 
         mv.addAttribute("title", "Spring profile: " + user.getName());
         mv.addAttribute("user", user);
 
+
       return "profile";
+
+    }
+
+    @ModelAttribute("userList")
+    public List<User> fillUsers(){
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("User1", "Last1","email1.g.c"));
+        userList.add(new User("User2", "Last2","email2.g.c"));
+        userList.add(new User("User3", "Last3","email3.g.c"));
+
+        return userList;
 
     }
 
