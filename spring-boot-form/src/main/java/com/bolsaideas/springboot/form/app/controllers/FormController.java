4c02a155cdb2882cc9596d19614a15662d9eca52
Daniel <djblancoh@gmail.com>
2022-12-01 17:07:11 -0500

    add validation, and set object to templates

--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,18 @@
 package com.bolsaideas.springboot.form.app.controllers;
 
 import com.bolsaideas.springboot.form.app.models.domain.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class FormController {
@@ -13,18 +20,21 @@ public class FormController {
     @GetMapping("/form")
     public String form(Model model){
         model.addAttribute("title", "User Form");
+        model.addAttribute("user", new User());
         return "index";
     }
 
     @PostMapping("/form")
-    public String processForm(User user, Model model
+    public String processForm(
+//            @Valid @ModelAttribute("userInput") User user,
+            @Valid User user,
+            BindingResult result, Model model
 //            ,
 //                              @RequestParam String username,
 //                              @RequestParam String password,
 //                              @RequestParam String email
                               ){
 
-        model.addAttribute("title", "Form result");
 //        model.addAttribute("userTemplate", username);
 //        model.addAttribute("passwordTemplate", password);
 //        model.addAttribute("emailTemplate", email);
@@ -35,7 +45,15 @@ public class FormController {
 //                .email(email)
 //                .build();
 
+        // check validations
+        if (result.hasErrors() ){
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach( fieldError -> errors.put(fieldError.getField(), "Field ".concat(fieldError.getField()).concat(" ").concat(Objects.requireNonNull(fieldError.getDefaultMessage()))));
+            model.addAttribute("errors", errors);
+            return "index";
+        }
 
+        model.addAttribute("title", "Form result");
         model.addAttribute("userModel", user);
 
         return "result";
--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/User.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/User.java
@@ -1,12 +1,23 @@
 package com.bolsaideas.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
+
+    @NotEmpty
     private String username;
+
+    @NotEmpty
     private String password;
+
+    @NotEmpty
     private String email;
 }
